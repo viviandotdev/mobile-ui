3bb47db2f024f917995bb608467d513432e7c229
vivianlin <linvivian61@gmail.com>
2025-09-30 22:01:31 -0400

    feat: add test button component to package and docs

--- packages/example-library/src/examples/index.ts
+++ packages/example-library/src/examples/index.ts
@@ -1,2 +1,3 @@
 export * from './red-button/red-button-primary';
 export * from './red-button/red-button-secondary';
+export * from './test-button';
---
+++ packages/example-library/src/examples/test-button.tsx
@@ -0,0 +1,5 @@
+import { TestButton } from '@/example-library/components/ui/test-button';
+
+export function TestButtonPreview() {
+    return <TestButton label="Test Button" />;
+}
